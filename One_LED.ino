a1714cacb1ed6cb0029f116f2e4fec2d0d17b460
brendanjduff <brendanjduff@gmail.com>
2022-03-24 13:37:49 -0400

    Initial Commit
    The code for Hello World (One LED blinking)

---
+++ One_LED.ino
@@ -0,0 +1,21 @@
+
+#define BUTTON 7
+#define LED1 8
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(BUTTON, INPUT);
+  pinMode(LED1, OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(BUTTON) == HIGH) {
+    digitalWrite(LED1, HIGH);
+    delay(500);
+    digitalWrite(LED1, LOW);
+    delay(500);
+  }
+  else {
+    digitalWrite(LED1, LOW);
+  }
+}
